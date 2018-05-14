6623113ad231c772d72304987ac87de953527605
Brent Perry <burentoperii@gmail.com>
2018-05-14 17:57:58 -0500

    Add operator event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,11 @@ function operate(operator, num1, num2) {
   }
 }
 
+let currentDisplay = "";
+// let operator;
+let operatorValue = 0;
+let totalValue = 0;
+
 const keys = document.querySelector('.keys');
 
 const display = document.querySelector('#display');
@@ -42,5 +47,27 @@ const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
   number.addEventListener('click', (e) => {
     display.textContent += e.target.value;
+    currentDisplay += e.target.value;
   });
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', (e) => {
+    operatorValue = parseFloat(currentDisplay);
+    let operator = e.target.value;
+    totalValue = operate(operator, totalValue, operatorValue);
+    currentDisplay = "";
+    display.textContent = '';
+  });
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', (e) => {
+  totalValue = operate(operator, totalValue, operatorValue);
+  currentDisplay.textContent = totalValue;
+  currentDisplay = "";
+  // operator = "";
+  operatorValue = 0;
+  // totalValue = 0;
 });
