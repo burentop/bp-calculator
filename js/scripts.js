194cdaa19b213276b18c2e9f5d2a415946b44afa
Brent Perry <burentoperii@gmail.com>
2018-05-15 19:40:44 -0500

    Add functionality to all keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,56 @@
+let currentDisplay = "";
+let operator = "";
+let operatorValue = 0;
+let totalValue = 0;
+
+function handleNumberKey(event) {
+  display.textContent += event.target.value;
+  currentDisplay += event.target.value;
+}
+
+function handleOperatorKey(event) {
+  operatorValue = parseFloat(currentDisplay);
+  operator = event.target.value;
+  totalValue = operate(operator, totalValue, operatorValue);
+  currentDisplay = "";
+  display.textContent = '';
+}
+
+function handleEqualsKey(event) {
+  operatorValue = parseFloat(currentDisplay);
+  totalValue = operate(operator, totalValue, operatorValue);
+  display.textContent = totalValue;
+  currentDisplay = "";
+  operator = "";
+  operatorValue = 0;
+  totalValue = 0;
+}
+
+function handleClearKey(event) {
+  display.textContent = "";
+}
+
+const keys = document.querySelector('.keys');
+
+const display = document.querySelector('#display');
+display.textContent = '';
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+  number.addEventListener('click', handleNumberKey, true);
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', handleOperatorKey, true);
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', handleEqualsKey, true);
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', handleClearKey, true);
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -33,41 +86,5 @@ function operate(operator, num1, num2) {
   }
 }
 
-let currentDisplay = "";
-// let operator;
-let operatorValue = 0;
-let totalValue = 0;
 
-const keys = document.querySelector('.keys');
 
-const display = document.querySelector('#display');
-display.textContent = '';
-
-const numbers = document.querySelectorAll('.number');
-numbers.forEach((number) => {
-  number.addEventListener('click', (e) => {
-    display.textContent += e.target.value;
-    currentDisplay += e.target.value;
-  });
-});
-
-const operators = document.querySelectorAll('.operator');
-operators.forEach((operator) => {
-  operator.addEventListener('click', (e) => {
-    operatorValue = parseFloat(currentDisplay);
-    let operator = e.target.value;
-    totalValue = operate(operator, totalValue, operatorValue);
-    currentDisplay = "";
-    display.textContent = '';
-  });
-});
-
-const equals = document.querySelector('#equals');
-equals.addEventListener('click', (e) => {
-  totalValue = operate(operator, totalValue, operatorValue);
-  currentDisplay.textContent = totalValue;
-  currentDisplay = "";
-  // operator = "";
-  operatorValue = 0;
-  // totalValue = 0;
-});
